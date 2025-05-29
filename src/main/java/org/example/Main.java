6e7740a867eafd47ba60b729e84e8f856a43c301
Hytralium <106311298+WeirdestBoi@users.noreply.github.com>
2025-05-30 00:33:02 +0200

    added command manager
    let me know if you need help using it

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,38 +1,9 @@
 package org.example;// importing JSON simple libraries
-import java.io.FileReader;
-import java.io.PrintWriter;
-import java.util.Iterator;
-import java.util.Map;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.*;
+
+import commandmanager.CommandManager;
 
 public class Main {
-    public static void main(String[] args) throws Exception
-    {
-        Object object1 = new JSONParser().parse(new FileReader("package.json"));
-
-        JSONObject jsonObject1 = (JSONObject)object1;
-
-        String name = (String)jsonObject1.get("name");
-        System.out.println(name);
-
-
-        JSONObject jsonObject2 = new JSONObject();
-
-        jsonObject2.put("name","John");
-
-        PrintWriter printWriter = new PrintWriter("package.json");
-        printWriter.write(jsonObject2.toJSONString());
-        printWriter.flush();
-        printWriter.close();
-
-
-        Object object3 = new JSONParser().parse(new FileReader("package.json"));
-
-        JSONObject jsonObject3 = (JSONObject)object3;
-
-        name = (String)jsonObject3.get("name");
-        System.out.println(name);
+    public static void main(String[] args) throws Exception {
+        CommandManager cm = new CommandManager();
     }
 }
